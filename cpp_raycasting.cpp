a979bda3df33ce08c0255fefff683ff86bc59f22
Affteron <Lontaredal@outlook.com>
2022-01-07 20:56:06 +0300

    Add save the framebuffer to file

--- cpp_raycasting.cpp
+++ cpp_raycasting.cpp
@@ -42,10 +42,10 @@ void unpack_color(const uint32_t &color, uint8_t &r, uint8_t &g, uint8_t &b, uin
     /**
      * For example:
      * color = 0x3264C8FF
-     * r = 0x3264C8FF >> 24 & 255 = 0b0011_0010_0110_0100_1100_1000_1111_1111 >> 24 & 0b0000_0000_0000_0000_0000_0000_1111_1111 = 0b0000_0000_0000_0000_0000_0000_0011_0010
-     * g = 0x3264C8FF >> 16 & 255 = 0b0011_0010_0110_0100_1100_1000_1111_1111 >> 16 & 0b0000_0000_0000_0000_0000_0000_1111_1111 = 0b0000_0000_0000_0000_0000_0000_0110_0100
-     * b = 0x3264C8FF >> 8 & 255 = 0b0011_0010_0110_0100_1100_1000_1111_1111 >> 8 & 0b0000_0000_0000_0000_0000_0000_1111_1111 = 0b0000_0000_0000_0000_0000_0000_1100_1000
-     * a = 0x3264C8FF >> 0 & 255 = 0b0011_0010_0110_0100_1100_1000_1111_1111 >> 24 & 0b0000_0000_0000_0000_0000_0000_1111_1111 = 0b0000_0000_0000_0000_0000_0000_1111_1111
+     * r = 0x3264C8FF >> 24 & 255 = 0b0011_0010_0110_0100_1100_1000_1111_1111 >> 24 & 0b0000_0000_0000_0000_0000_0000_1111_1111 = 0b0011_0010
+     * g = 0x3264C8FF >> 16 & 255 = 0b0011_0010_0110_0100_1100_1000_1111_1111 >> 16 & 0b0000_0000_0000_0000_0000_0000_1111_1111 = 0b0110_0100
+     * b = 0x3264C8FF >> 8 & 255 = 0b0011_0010_0110_0100_1100_1000_1111_1111 >> 8 & 0b0000_0000_0000_0000_0000_0000_1111_1111 = 0b1100_1000
+     * a = 0x3264C8FF >> 0 & 255 = 0b0011_0010_0110_0100_1100_1000_1111_1111 >> 24 & 0b0000_0000_0000_0000_0000_0000_1111_1111 = 0b1111_1111
      */                             
     r = (color >> 24) & 255;
     g = (color >> 16) & 255;
@@ -53,26 +53,54 @@ void unpack_color(const uint32_t &color, uint8_t &r, uint8_t &g, uint8_t &b, uin
     a = (color >> 0) & 255;
 }
 
-int main() {
-    // DEBUG
-    int a = 50;
-    int pow = 24;
-    std::string a_binary = (std::bitset<32>(a)).to_string();
-    std::string a_binary_w = (std::bitset<32>(a) << pow).to_string();
-    
-    unsigned long a_decimal = std::bitset<32>(a_binary).to_ulong();
-    unsigned long a_decimal_w = std::bitset<32>(a_binary_w).to_ulong();
-    
-    std::stringstream ss;
-    ss << std::hex << a_decimal_w;
-    std::string a_hex (ss.str());
+/**
+ * @brief Save image to PPM (P6) format on disk
+ * 
+ * @param filename path to output file
+ * @param image ref to image's buffer
+ * @param w image width 
+ * @param h image heigth
+ */
+void drop_ppm_image(const std::string filename, const std::vector<uint32_t> &image, const size_t w, const size_t h) {
+    /**
+     * Code of PPM file's example:
+     * 
+     * P6                       <-- Format (magic number)
+     * # Some comment           <-- Comment
+     * 3 2                      <-- Image Size (Width&Heigth)
+     * 255                      <-- Max color's value
+     * !@#$%^&*()_+|{}:"<       <-- Each three symbol equale RGB value.
+     */
+    assert(image.size() == w * h);
+    std::ofstream ofs(filename, std::ios::binary);
+    ofs << "P6\n" << w << " " << h << "\n255\n";
+    for (size_t i = 0; i < h * w; i++) {
+        uint8_t r, g, b, a;
+        unpack_color(image[i], r, g, b, a);
+        ofs << static_cast<char>(r) << static_cast<char>(g) << static_cast<char>(b);
+    }
+    ofs.close();
+}
 
-    std::cout << a_binary << std::endl;
-    std::cout << a_binary_w << std::endl;
-    std::cout << a_decimal << std::endl;
-    std::cout << a_decimal_w << std::endl;
-    std::cout << a_hex << std::endl;
-    // DEBUG
+int main() {
+    // Image width
+    const size_t win_w = 512;
+    // Image heigth
+    const size_t win_h = 512;
+    // Image buffer, initialized  to red
+    std::vector<uint32_t> framebuffer(win_w*win_h, 255);
+
+    // Fill the screen with color gradient
+    for(size_t j = 0; j < win_h; j++) {
+        for(size_t i = 0; i < win_w; i++) {
+            uint8_t r = 255 * j / float(win_h);
+            uint8_t g = 255 * i / float(win_h);
+            uint8_t b = 0;
+            framebuffer[i + j * win_w] = pack_color(r, g, b);
+        }
+    }
+
+    drop_ppm_image("./out.ppm", framebuffer, win_w, win_h);
 
     return 0;
 }
